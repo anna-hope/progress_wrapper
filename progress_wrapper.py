6e885cf4d112d97c7538aa8df67d5d68b6cd2469
Anton Osten <vizardx@gmail.com>
2016-09-06 14:14:51 -0500

    Initial commit

---
+++ progress_wrapper.py
@@ -0,0 +1,38 @@
+# (c) Anton Melnikov 2016
+
+class ProgressWrapper:
+    """
+    Wraps an iterable and returns an iterable which acts like
+    the original iterable and updates the given progress bar
+    in the process.
+    """
+
+    def __init__(self, iterable, progressbar, total=None):
+        """
+        Parameters
+        :param iterable: iterable
+        :param progressbar:
+            A progress bar object. It has to have an attribute "total"
+            and a method "update([n])"
+        :param total: int
+            The length of the iterabld
+        """
+        self.iterable = iterable
+        self.progressbar = progressbar
+
+        if total:
+            self.total = total
+        else:
+            try:
+                self.total = len(iterable)
+            except TypeError:
+                raise ValueError('Could not determine the length of the iterable, '
+                                 + 'and no total was given.')
+
+        self.progressbar.total = self.total
+
+
+    def __iter__(self):
+        for element in self.iterable:
+            yield element
+            self.progressbar.update()
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='progress_wrapper',
+      version='0.1',
+      description='A wrapper for iterables that updates progress bars',
+      author='Anton Melnikov',
+      author_email='the.notnami@gmail.com',
+      py_modules=['progress_wrapper'])
